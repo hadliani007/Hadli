79a58ca441dfd50e73fda761999ae5f3d38f06fc
hadliani007 <32351403+hadliani007@users.noreply.github.com>
2017-10-25 20:05:18 +0800

    luar 1 box parket

--- praktek0204.cpp
+++ praktek0204.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main(){
 	int LR=42;
-	cout<<"luas rumah		: "<<LR<<" m2 "<<endl;
+	cout<<"luas rumah			: "<<LR<<" m2 "<<endl;
 	double LP=1.87596;
+	cout<<"luas 1 box parket	: "<<LP<<" m2 "<<endl;
+	double JP=LR/LP;	
 }
