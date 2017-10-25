3d53adea242c278c3da43f8deae15192dcdac733
hadliani007 <32351403+hadliani007@users.noreply.github.com>
2017-10-25 19:06:03 +0800

    perbaikan

--- Praktek0201.cpp
+++ Praktek0201.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 int main (){
-	nilai=nilai+1;
 	int nilai;
-	int 1Nilai;
+	int Nilai = 1;
 	
-	Cout << nilai;
-	cout >> 1Nilai;
+	cout << "Masukan nilai = " ;
+	cin >> nilai;
+	nilai= nilai + Nilai;
+	
+	cout << nilai;
 	
 }
